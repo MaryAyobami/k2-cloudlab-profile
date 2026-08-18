5a187b57098c706a0bcaf1ea8e2d8a73ac862a37
Mary Ayobami Ogunmola <ogunmolamaryayobami@gmail.com>
2026-08-18 14:43:37 -0600

    Enhance CloudLab profile with per-node hardware types
    Updated the CloudLab profile to support per-node selectable hardware types and improved parameter definitions for DUT and generator nodes.

--- profile.py
+++ profile.py
@@ -1,66 +1,98 @@
-"""K2 sigcomm21 artifact CloudLab profile with selectable node type."""
+"""K2 sigcomm21 artifact CloudLab profile with per-node selectable hardware type."""
+
 import geni.portal as portal
 import geni.rspec.igext as IG
-import geni.rspec.emulab
+import geni.rspec.emulab  # registers setNoInterSwitchLinks on Link
 
 AUTHOR_DUT = "urn:publicid:IDN+utah.cloudlab.us+image+heartbeat-PG0:xl170-centos7-ubuntu20:2"
 AUTHOR_GEN = "urn:publicid:IDN+utah.cloudlab.us+image+heartbeat-PG0:xl170-centos7-ubuntu20.node-1:5"
 STOCK_DUT = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD"
 STOCK_GEN = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD"
-RESTORE_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+STOCK_NEW = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 
 NODES = ["xl170", "c6525-25g", "sm110p", "r6615", "d7615", "c6525-100g", "c6620", "d760p"]
-LINK_BANDWIDTH = 25000000
+
+IMAGES = [("authors", "Authors' xl170 snapshots"),
+          ("stock", "Stock Ubuntu 20.04 + CentOS 7"),
+          ("ubuntu22", "Stock Ubuntu 22.04")]
+
+
+LINK_RATES = [("25000000", "25 Gbps"), ("100000000", "100 Gbps")]
 
 pc = portal.Context()
-pc.defineParameter("node_type", "Node type", portal.ParameterType.STRING, NODES[0],
-                   legalValues=[(name, name) for name in NODES])
-pc.defineParameter("images", "Disk images", portal.ParameterType.STRING, "authors",
-                   legalValues=[("authors", "Authors' images (xl170 only)"),
-                                ("stock", "Stock Ubuntu 20.04 + CentOS 7"),
-                                ("restore", "Ubuntu 22.04 on both nodes")])
+
+pc.defineParameter("dut_type", "DUT node type (node-0)", portal.ParameterType.STRING, NODES[0],
+                   legalValues=[(n, n) for n in NODES])
+
+pc.defineParameter("dut_images", "DUT disk image", portal.ParameterType.STRING, "authors",
+                   legalValues=IMAGES)
+
+pc.defineParameter("gen_type", "Traffic generator node type (node-1)", portal.ParameterType.STRING,
+                   NODES[0], legalValues=[(n, n) for n in NODES])
+
+pc.defineParameter("gen_images", "Generator disk image", portal.ParameterType.STRING, "authors",
+                   legalValues=IMAGES)
+
+pc.defineParameter("link_rate", "Experiment link rate", portal.ParameterType.STRING,
+                   LINK_RATES[0][0], legalValues=LINK_RATES)
+
 pc.defineParameter("same_switch", "Keep both nodes on one switch",
                    portal.ParameterType.BOOLEAN, True, advanced=True)
+
 pc.defineParameter("gen_public_ip", "Routable control IP on node-1",
                    portal.ParameterType.BOOLEAN, False, advanced=True)
+
 params = pc.bindParameters()
 
-if params.images == "authors" and params.node_type != "xl170":
-    pc.reportError(portal.ParameterError(
-        "Authors' images are xl170 snapshots; select stock or restore images for %s." % params.node_type,
-        ["images", "node_type"]))
+
+for role, ntype, imgs in (("DUT", params.dut_type, params.dut_images),
+                          ("generator", params.gen_type, params.gen_images)):
+    if imgs == "authors" and ntype != "xl170":
+        pc.reportWarning(portal.ParameterWarning(
+            "Authors' images are xl170 snapshots; on %s (%s) CloudLab may refuse to map them. "
+            "Use ubuntu22 and switch OS via GRUB if it does." % (ntype, role), []))
+
+
+if params.same_switch and params.dut_type != params.gen_type:
+    pc.reportWarning(portal.ParameterWarning(
+        "DUT and generator are different node types; same-switch placement will likely fail. "
+        "Disable 'Keep both nodes on one switch' if mapping is refused.", ["same_switch"]))
 pc.verifyParameters()
 
-if params.images == "authors":
-    dut_img, gen_img = AUTHOR_DUT, AUTHOR_GEN
-elif params.images == "stock":
-    dut_img, gen_img = STOCK_DUT, STOCK_GEN
-else:
-    dut_img, gen_img = RESTORE_IMG, RESTORE_IMG
+
+def image_for(choice, role_default):
+    if choice == "authors":
+        return role_default
+    if choice == "ubuntu22":
+        return STOCK_NEW
+    return STOCK_DUT if role_default is AUTHOR_DUT else STOCK_GEN
+
 
 request = pc.makeRequestRSpec()
 
 node0 = request.RawPC("node-0")
-node0.hardware_type = params.node_type
-node0.disk_image = dut_img
+node0.hardware_type = params.dut_type
+node0.disk_image = image_for(params.dut_images, AUTHOR_DUT)
 node0.routable_control_ip = True
 iface0 = node0.addInterface("interface-0")
 
 node1 = request.RawPC("node-1")
-node1.hardware_type = params.node_type
-node1.disk_image = gen_img
+node1.hardware_type = params.gen_type
+node1.disk_image = image_for(params.gen_images, AUTHOR_GEN)
 if params.gen_public_ip:
     node1.routable_control_ip = True
 iface1 = node1.addInterface("interface-1")
 
 link = request.Link("link-0", members=[iface0, iface1])
-link.bandwidth = LINK_BANDWIDTH
+link.bandwidth = int(params.link_rate)
 if params.same_switch:
     link.setNoInterSwitchLinks()
 
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN,
-                 "Traffic generator + DUT setup with %s machines (%s images)" %
-                 (params.node_type, params.images))
+                 "K2 latency/throughput setup: DUT on %s, T-Rex traffic generator on %s, "
+                 "%s link" % (params.dut_type, params.gen_type,
+                              dict(LINK_RATES)[params.link_rate]))
 request.addTour(tour)
+
 pc.printRequestRSpec(request)
